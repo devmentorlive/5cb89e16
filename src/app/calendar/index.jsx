e11027a04591fda23b270a48eacd8c3bf883eb58
Mark Tellez <mark.tellez@icloud.com>
2020-08-27 11:48:17 -0500

    calendar header

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,112 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import moment from "moment";
-import "./styles.css";
 
-export default function Calendar({ value, onChange }) {
-  const [calendar, setCalendar] = useState([]);
+export default function Calendar() {
+  const value = moment();
+  const startDay = value.clone().startOf("month").startOf("week");
+  const endDay = value.clone().endOf("month").endOf("week");
+  const day = startDay.clone().subtract(1, "day");
 
-  useEffect(() => {
-    setCalendar(buildCalendar(value));
-  }, [value]);
+  const calendar = [];
 
-  function buildCalendar(date) {
-    const a = [];
-
-    const startDay = date.clone().startOf("month").startOf("week");
-    const endDay = date.clone().endOf("month").endOf("week");
-
-    const _date = startDay.clone().subtract(1, "day");
-
-    while (_date.isBefore(endDay, "day")) {
-      a.push(
-        Array(7)
-          .fill(0)
-          .map(() => _date.add(1, "day").clone())
-      );
-    }
-    return a;
-  }
-
-  function isSelected(day) {
-    return value.isSame(day, "day");
-  }
-
-  function beforeToday(day) {
-    return moment(day).isBefore(new Date(), "day");
-  }
-
-  function isToday(day) {
-    return moment(new Date()).isSame(day, "day");
-  }
-
-  function dayStyles(day) {
-    if (beforeToday(day)) return "before";
-    if (isSelected(day)) return "selected";
-    if (isToday(day)) return "today";
-    return "";
-  }
-
-  function currMonthName() {
-    return value.format("MMMM");
-  }
-
-  function currYear() {
-    return value.format("YYYY");
-  }
-
-  return (
-    <div className="calendar">
-      <div className="header">
-        <div className="previous">
-          <a
-            onClick={() => {
-              const newDate = value.clone().subtract(1, "month");
-              onChange(newDate);
-            }}
-          >
-            {String.fromCharCode(171)}
-          </a>
-        </div>
-        <div className="current">
-          {currMonthName()} {currYear()}
-        </div>
-        <div className="next">
-          <a
-            onClick={() => {
-              const newDate = value.clone().add(1, "month");
-              onChange(newDate);
-            }}
-          >
-            {String.fromCharCode(187)}
-          </a>
-        </div>
-      </div>
-
-      <div className="body">
-        <div className="day-names">
-          {["s", "m", "t", "w", "t", "f", "s"].map((d) => (
-            <div className="week">{d}</div>
-          ))}
-        </div>
-        {calendar.map((week, wi) => (
-          <div key={wi}>
-            {week.map((day, di) => (
-              <div
-                key={di}
-                className="day"
-                onClick={() => {
-                  if (day < moment(new Date()).startOf("day")) return;
-                  onChange(day);
-                }}
-              >
-                <div className={dayStyles(day)}>
-                  {day.format("D").toString()}
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div>{day.format("MM/DD")}</div>;
 }
