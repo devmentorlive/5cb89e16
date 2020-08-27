9b71447007d1fc84706c1e1be70f044e43a1b7a6
Mark Tellez <mark.tellez@icloud.com>
2020-08-27 11:48:17 -0500

    calendar header

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
-import moment from "moment";
+import React from "react";
 import Calendar from "./calendar";
 import "./styles.css";
 
 export default function () {
-  const [selectedDate, setSelectedDate] = useState(moment());
-  return <Calendar value={selectedDate} onChange={setSelectedDate} />;
+  return <Calendar />;
 }
