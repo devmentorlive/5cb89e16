34101ea6e8d18b9c213a93b761cc02bc6c6ba457
Mark Tellez <mark.tellez@icloud.com>
2020-08-30 09:07:23 -0500

    done

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,24 +1,14 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import "./styles.css";
+import buildCalendar from "./build";
 
 export default function Calendar() {
   const [calendar, setCalendar] = useState([]);
   const [value, setValue] = useState(moment());
-  const startDay = value.clone().startOf("month").startOf("week");
-  const endDay = value.clone().endOf("month").endOf("week");
 
   useEffect(() => {
-    const day = startDay.clone().subtract(1, "day");
-    const a = [];
-    while (day.isBefore(endDay, "day")) {
-      a.push(
-        Array(7)
-          .fill(0)
-          .map(() => day.add(1, "day").clone())
-      );
-    }
-    setCalendar(a);
+    setCalendar(buildCalendar(value));
   }, [value]);
 
   return (
